dfc90898197cbe2cfd2e9317a694b1e12cb8d56f
Johannes <johannes@johannesskagius@gmail.com>
2021-03-11 16:57:39 +0100

    Kollar på att effektivisera score. Ny klass Score2.java som sedan ersättare Score.java

--- src/com/company/Score2.java
+++ src/com/company/Score2.java
@@ -1,28 +1,33 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Score2 {
     private Board b;
     private int boardRowSize;
     private int inARowToWin;
     private Node[][] playField;
+    private List<Coordinate> playedPos = new ArrayList<> ();
 
     public Score2 (Board b) {
         this.b = b;
         this.playField = b.getPlayField ();
         boardRowSize = b.getPLAYFIELDSIZE ();
         inARowToWin = b.getROW_TO_WIN ();
+        playedPos = b.getPlayedPositions ();
     }
 
-    public int getScore(){
-        int humanScore = calcScore(Node.Brick.HUMAN );
-
-        int computerScore = calcScore( Node.Brick.COMPUTER );
-        return computerScore - humanScore;
-    }
-
-    private int calcScore (Node.Brick player) {
-
+    public int getScore (String player) {
+        //getVerticalScore
+        int vertical = getVerticalScore(player);
+        //getHorizontalScore
+        //getDiagonalScore
         return 0;
     }
 
+    private int getVerticalScore (String player) {
+        playedPos.get ( 0 );
+
+    }
 }
